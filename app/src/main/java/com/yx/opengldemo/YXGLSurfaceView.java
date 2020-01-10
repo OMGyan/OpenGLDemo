1dfa878bbe22cc80ab4bd923c973f11bf612674b
yanxun <1768695841@qq.com>
2020-01-10 11:40:05 +0800

    展示OpenGL渲染

---
+++ app/src/main/java/com/yx/opengldemo/YXGLSurfaceView.java
@@ -0,0 +1,24 @@
+package com.yx.opengldemo;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+/**
+ * Author by YX, Date on 2020/1/9.
+ *
+ * 用于OpenGL渲染
+ */
+public class YXGLSurfaceView extends GLSurfaceView{
+    public YXGLSurfaceView(Context context) {
+        this(context,null);
+    }
+
+    public YXGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        //通知默认的EGLContextFactory和默认的EGLConfigChooser,选择哪个EGLContext客户端版本
+        setEGLContextClientVersion(2);
+        //设置与此视图关联的渲染器。同时启动那个线程,将调用渲染器，这又将导致渲染开始.
+        setRenderer(new YXRenderer(context));
+    }
+}
